e5c8fda469f4b85b9302b7cb68374e8cab328e2b
Nata <natka9017@mail.ru>
2025-01-03 22:55:39 +1000

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    int price = 17_676; // цена билета
+    int bonus = price / 20; // бонусы
+    System.out.printf("Бонус: " +bonus);
+    }
+
+}

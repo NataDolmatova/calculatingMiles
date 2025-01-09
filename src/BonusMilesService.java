684b2905ed44c9002ecbe4d0f1ef8118528e3298
Nata <natka9017@mail.ru>
2025-01-09 21:33:49 +1000

    Initiation commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int bonus = price / 30;
+        return bonus;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    int price = 17_676; // цена билета
-    int bonus = price / 20; // бонусы
-    System.out.printf("Бонус: " +bonus);
-    }
-
+    BonusMilesService service = new BonusMilesService();
+    int price = 15_198;
+    int miles = service.calculate(price); // должно получиться 500
+    System.out.println(miles);
+  }
 }
